e6733c11254e7ed9d17191d79f147ae57867aee7
IMJIHWAN <ikuwlghks@naver.com>
2023-11-15 02:15:00 +0900

    jihwan

--- src/client/Client.java
+++ src/client/Client.java
@@ -7,10 +7,11 @@ import javax.swing.JPanel;
 import client.DetailMyGroup.DetailMyGroup;
 import client.MainPage.MainPage;
 import client.mypage.Mypage;
+import client.recruitpage.RecruitGroupMember;
 
 public class Client {
 	
 	  public static void main(String [] args) {
-	      Mypage mypage = new Mypage(true);
+	      RecruitGroupMember m = new RecruitGroupMember(true);
 	   }
 }
