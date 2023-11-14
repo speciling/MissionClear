e97168c23abf270521fd5a85bd0b08dbee372dd3
u_u <yw020628@gmail.com>
2023-11-14 19:42:44 +0900

    아무거나

--- src/client/Client.java
+++ src/client/Client.java
@@ -1,4 +1,33 @@
 package client;
 
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import client.DetailMyGroup.DetailMyGroup;
+import client.MainPage.MainPage;
+
+
+
 public class Client {
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	public static void main(String [] args) {
+		DetailMyGroup dm = new DetailMyGroup(true);
+	}
+	
+	
+	
+	
+	
+	
 }
