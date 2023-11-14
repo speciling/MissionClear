9fddf28f56982b3fa821cf27a99a278798b4ba9f
speciling <speciling@naver.com>
2023-11-14 23:09:46 +0900

    DBManager 수정
    멤버함수 반환값들 JSON객체로 수정, 클라이언트에서도 사용되는 기능들을 상속해서 사용할 수 있도록 수정

--- src/server/service/Request.java
+++ src/server/service/Request.java
@@ -1,19 +1,21 @@
 package server.service;
 
 import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
 import java.nio.ByteBuffer;
 import java.util.Collections;
 import java.util.Map;
+import java.util.Objects;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Request {
     public RequestType type;
     public String bodyString;
-    public JSONArray data;
+    private JSONObject data;
 
     public Request(byte type, String body) {
         this.type = RequestType.of(type);
@@ -26,7 +28,9 @@ public class Request {
         }
     }
 
-    public static ByteBuffer getPacket(RequestType type, JSONArray data) {
+    public JSONObject getData() { return data; }
+
+    public static ByteBuffer getPacket(RequestType type, JSONObject data) {
         return null;
     }
 
