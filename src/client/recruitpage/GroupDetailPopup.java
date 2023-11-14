4c2c4ab6f7abf28b429f17a7281d9089bd7b0794
IMJIHWAN <ikuwlghks@naver.com>
2023-11-15 02:31:39 +0900

    jihwan

--- src/client/recruitpage/GroupDetailPopup.java
+++ src/client/recruitpage/GroupDetailPopup.java
@@ -28,19 +28,6 @@ public class GroupDetailPopup {
 	/**
 	 * Launch the application.
 	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					GroupDetailPopup window = new GroupDetailPopup();
-					window.frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
 	/**
 	 * Create the application.
 	 */
