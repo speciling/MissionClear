089c56416585e30458ebc802500378be8c184e8f
speciling <speciling@naver.com>
2023-11-30 15:40:41 +0900

    Update Client.java

--- src/client/Client.java
+++ src/client/Client.java
@@ -4,15 +4,21 @@ import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
+import client.db.ClientDBManager;
 import client.detailMyGroup.DetailMyGroup;
 import client.MainPage.MainPage;
+import client.login.Login;
 import client.mypage.Mypage;
+import client.net.ClientSocket;
 import client.recruitpage.MyGroupList;
 import client.recruitpage.RecruitGroupMember;
 
 public class Client {
-	/*
 	  public static void main(String [] args) {
-		  MainPage a = new MainPage(true);
-	   }*/
+          ClientSocket socket = new ClientSocket(8080);
+          socket.start();
+          ClientDBManager.init();
+          Login login = new Login();
+          login.loginpage();
+	   }
 }
