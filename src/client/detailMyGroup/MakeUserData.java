2e1e1ac769276a3acfbfc868d94744e5a8c24498
u_u <yw020628@gmail.com>
2023-12-02 00:56:50 +0900

    tw
    asdg

--- src/client/detailMyGroup/MakeUserData.java
+++ src/client/detailMyGroup/MakeUserData.java
@@ -16,22 +16,15 @@ public class MakeUserData {
 	
 	
 	public MakeUserData() {
-		System.out.println(11111);
 		JSONArray usersData = ClientDBManager.getGroupUsers(gid);
-		System.out.println(usersData);
 		for(int i=0;i<usersData.size();i++) {		
 			JSONObject user = (JSONObject) usersData.get(i);		
 			int uid = Integer.parseInt(user.get("uid").toString());	
-			System.out.println(uid);
-			
 			uids.add(uid);
 			String nickname = user.get("nickname").toString();		
-			
-			System.out.println(nickname);
 			nicknames.put(uid,nickname);	
 			String pfp = user.get("pfp").toString();
 			if(pfp.equals("")) {pfp = "./resource/MainPage/defaultPic.png";}
-			System.out.println(pfp);
 			pfps.put(uid,pfp);
 		}
 	}
