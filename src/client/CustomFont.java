1ea66fb477ee6b93f3b81c8e7e3322eb3b0d77da
jiiiwonyy <ji__Won_22@naver.com>
2023-12-02 16:08:36 +0900

    프로필 사진 수정

---
+++ src/client/CustomFont.java
@@ -0,0 +1,32 @@
+package client;
+
+import java.awt.*;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class CustomFont {
+/*
+    private static final String FONT_PATH = "/NanumGothic.ttf";
+    private Font font;
+
+    public CustomFont() {
+        try (InputStream fontStream = getClass().getClassLoader().getResourceAsStream(FONT_PATH)) {
+            // InputStream으로부터 Font 객체 생성
+            font = Font.createFont(Font.TRUETYPE_FONT, fontStream);
+            GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+            ge.registerFont(font);
+        } catch (IOException | FontFormatException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Font getFont() {
+        return font;
+    }
+
+    public Font deriveFont(float size) {
+        return font.deriveFont(size);
+    }
+
+*/
+}
